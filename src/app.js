fdd5fb907f90d3dade44d0fec149726be142550e
shashwatrajhack <er.shashwatraj@gmail.com>
2024-12-10 22:50:29 +0530

    routes for project

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.use("/",(req,res)=>{
+    res.send("helooo from dashboard")
+});
+
+app.use("/test",(req,res)=>{
+    res.send("helooooo from test route")
+})
+app.use("/test",(req,res)=>{
+    res.send("helooooo from test route")
+})
+
 app.listen(3000);
