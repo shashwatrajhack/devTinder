8b05394a27a234b811dff596542f06872d6ffe9d
shashwatrajhack <er.shashwatraj@gmail.com>
2024-12-11 22:34:53 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("helooo from dashboard")
+// app.use("/",(req,res)=>{
+//     res.send("helooo from dashboard")
+// });
+
+app.use("/test",(req,res)=>{
+    res.send("helooooo from test route")
 });
 
-app.use("/test",(req,res)=>{
-    res.send("helooooo from test route")
-})
-app.use("/test",(req,res)=>{
-    res.send("helooooo from test route")
-})
-
-app.listen(3000);
+app.listen(7777);
