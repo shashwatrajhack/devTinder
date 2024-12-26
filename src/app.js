e97f9ae40672f8e41ea8fc5a5afacadd2fbdd875
shashwatrajhack <er.shashwatraj@gmail.com>
2024-12-27 00:08:17 +0530

    Database connection using momngoose

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
 const express = require("express");
+
+const connectDb = require("./config/database");
 const app = express();
 
 // app.use("/",(req,res)=>{
 //     res.send("helooo from dashboard")
 // });
 
-app.use("/admin/getAllData",
-    (req,res)=>{
-        //route handler
-        
+connectDb().then(() => {
+    console.log("Connection established successfully");
+    app.listen(7777,()=>{
+        console.log("server is successfully running on port 7777")
+    });
+})
+.catch((err) =>{
+     console.error("Connection not established successfully");
 
-    res.send("all data sent")
-});
+})
 
-app.listen(7777,()=>{
-    console.log("server is successfully running on port 7777")
-});
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDb = async () => {
+  await mongoose.connect(
+    "mongodb+srv://admin:ZE1B5Kiwfit3uF81@appdb.cdmoc.mongodb.net/"
+  );
+};
+
+module.exports = connectDb;
+
+
