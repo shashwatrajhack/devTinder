3928d28a4bd680ec724e0ac277e1bfb87d471023
shashwatrajhack <er.shashwatraj@gmail.com>
2024-12-27 12:20:18 +0530

    database added successfully

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@ const express = require("express");
 
 const connectDb = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
 // app.use("/",(req,res)=>{
 //     res.send("helooo from dashboard")
 // });
+
+app.post("/signup",async (req,res)=>{
+//creating an instance of user
+
+const user = new User({
+    firstName:"krishna",
+    lastName:"raj",
+    emailId:"krishna@gmail.com",
+    password:"krishna@1234"
+});
+
+try{
+    await user.save();
+res.send("user added successfully")
+}catch(err){
+    res.status(400).send("error saving the user:" + err.message)
+}
+})
 
 connectDb().then(() => {
     console.log("Connection established successfully");
