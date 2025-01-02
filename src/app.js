1223fac4df643f0d79def12061b69022861e9e2c
shashwatrajhack <er.shashwatraj@gmail.com>
2025-01-02 23:54:56 +0530

    login route and credentials matching

--- src/app.js
+++ src/app.js
@@ -38,6 +38,26 @@ res.send("user added successfully")
 }
 })
 
+app.post("/login",async (req,res)=>{
+    try{
+        const {emailId,password} = req.body;
+        const user = await User.findOne({emailId:emailId});
+        if(!user){
+            throw new error("invalid credentials");
+
+        }
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        if(isPasswordValid){
+            res.send("user login successful");
+        }else{
+            throw new error("invalid credentials");
+        }
+    }catch(err){
+        res.status(400).send("ERROR :" + err.message);
+    }
+
+})
+
 //get user by email
 app.get("/user",async (req,res) => {
     const userEmail = req.body.emailId;
