910d21a68c4214a16a0f5460cd776be90c9b5e3c
shashwatrajhack <er.shashwatraj@gmail.com>
2025-09-23 18:53:45 +0530

    adding db to the project

--- src/app.js
+++ src/app.js
@@ -1,24 +1,35 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+const connectDB = require("./config/database");
+const User = require("./models/user");
 
-//works for  /ac , /abc also
-app.get("/ab?c",(req,res) => {
-  res.send("Getting all the data from DB");
-})
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Shashwat",
+    lastName: "Raj",
+    Gender: "Male",
+    email: "Shashwat@gmail.com",
+    age: "22",
+  });
 
-app.get("/user",(req,res) => {
-  res.send("Getting all the data from DB");
-})
-
-app.post("/user",(req,res) => {
-  console.log("adding data to db");
-  res.send("Data added successfully to DB");
-})
-
-
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (err) {
+    res.status(400).send("error saving the user");
+  }
 });
+
+connectDB()
+  .then(() => {
+    console.log("database connection established");
+
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection cannot established");
+  });
---
+++ src/config/database.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://admin:ZE1B5Kiwfit3uF81@appdb.cdmoc.mongodb.net/devTinder")
+}
+
+module.exports = connectDB;
+
