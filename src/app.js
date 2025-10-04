9523916cfdd5628b1873dced1121845a0fe9eb51
shashwatrajhack <er.shashwatraj@gmail.com>
2025-10-04 18:29:40 +0530

    shema level validation added

--- src/app.js
+++ src/app.js
@@ -44,17 +44,19 @@ app.post("/login", async (req, res) => {
     if (!user) {
       throw new Error("Invalid Credentials");
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword(password);
 
     if (isPasswordValid) {
       // create a jwt token
-      const token = await jwt.sign({ _id: user._id }, "devTinder789");
+      const token = await user.getJWT();
 
       //Add the token to cookie and send the response back to user
 
       //cookie is like a temporary password which will come in all the request to the server
 
-      res.cookie("token", token);
+      res.cookie("token", token,{
+        expires: new Date(Date.now() + 8 * 360000)
+      });
 
       res.send("Login Successfull!!!");
     } else {
@@ -76,75 +78,11 @@ app.get("/profile",userAuth, async (req, res) => {
   }
 });
 
-app.get("/user", async (req, res) => {
-  const userEmail = req.body.emailId;
-
-  try {
-    const user = await User.find({ emailId: userEmail });
-    res.send(user);
-  } catch (err) {
-    console.log(err);
-    res.status(400).send("user not found");
-  }
-});
-
-app.get("/feed", async (req, res) => {
-  try {
-    const user = await User.find();
-    res.send(user);
-  } catch (err) {
-    console.log(err);
-    res.status(400).send("users not found : " + err);
-  }
-});
-
-app.delete("/user", async (req, res) => {
-  let userId = req.body.userId;
-
-  try {
-    const user = await User.findByIdAndDelete({ _id: userId });
-    res.send("user deleted successfully");
-  } catch (err) {
-    res.status(400).send("something went wrong");
-  }
-});
-
-app.patch("/user/:userId", async (req, res) => {
-  const userId = req.params?.userId;
-  const data = req.body;
-
-  try {
-    const ALLOWED_UPDATES = [
-      "firstName",
-      "lastName",
-      "photoURL",
-      "about",
-      "gender",
-      "age",
-      "skills",
-    ];
-
-    const isUpdateAllowed = Object.keys(data).every((k) =>
-      ALLOWED_UPDATES.includes(k)
-    );
-
-    if (!isUpdateAllowed) {
-      throw new Error("Update not Allowed");
-    }
-
-    if (data.skills.length > 10) {
-      throw new Error("Skill cannot be more than 10");
-    }
-    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
-      returnDocument: "after",
-      //to put validation during update use runvalidators
-      runValidators: true,
-    });
-    res.send("User updated successfully");
-  } catch (err) {
-    res.status(400).send("Something went wrong : " + err.message);
-  }
-});
+app.post("/sendConnectionRequest",userAuth,(req,res) => {
+  const user = req.user;
+ 
+  res.send(user.firstName + " Sent connection request");
+})
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,68 +1,92 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const jwt  = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    min: 4,
-    max: 50,
-  },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value){
-      if(!validator.isEmail(value)){
-        throw new Error("email address not valid "+value);
-      }
-    }
-  },
-  password: {
-    type: String,
-     validate(value){
-      if(!validator.isStrongPassword(value)){
-        throw new Error("Enter a strong password"+value);
-      }
-    }
-  },
-  age: {
-    type: Number,
-    min: 18,
-  },
-  gender: {
-    type: String,
-    lowercase:true,
-    //custom validation
-    validate(value) {
-      if (!["male", "female", "others"].includes(value)) {
-        throw new Error("gender is not valid");
-      }
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      min: 4,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("email address not valid " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a strong password" + value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      lowercase: true,
+      //custom validation
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("gender is not valid");
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: "This is default info of user",
+    },
+    photoURL: {
+      type: String,
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("URL address not valid " + value);
+        }
+      },
+    },
+    skills: {
+      type: [String],
     },
   },
-  about: {
-    type: String,
-    default: "This is default info of user",
-  },
-  photoURL: {
-    type: String,
-     validate(value){
-      if(!validator.isURL(value)){
-        throw new Error("URL address not valid "+value);
-      }
-    }
-  },
-  skills:{
-    type:[String],
+  {
+    timestamps: true,
   }
-},{
-  timestamps:true,
-},);
+);
+
+userSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id }, "devTinder789", {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+  const user = this;
+  const passwordHash = user.password;
+
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash
+  );
+  return isPasswordValid;
+};
 
 const User = mongoose.model("User", userSchema);
 
